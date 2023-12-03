c86af2fb16572bdab3a2a890adea21267b17b3b0
nahlasamy773 <nahlamostafahassan773@gmail.com>
2023-12-04 01:33:52 +0200

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    
+
+
+    function getData (Request $req){
+        return $req ;
+        
+       
+    
+
+    }
+
+    
+}
